7f8fe9c6d342a5530b2c64bbf6226d7aea7aa582
Paul Thomas <paulthomas2107@gmail.com>
2022-09-09 14:24:17 +0100

    Pre DB added

--- src/main/java/io/getarrays/server/ServerApplication.java
+++ src/main/java/io/getarrays/server/ServerApplication.java
@@ -1,7 +1,14 @@
 package io.getarrays.server;
 
+import io.getarrays.server.enumerations.Status;
+import io.getarrays.server.model.Server;
+import io.getarrays.server.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.beans.BeanProperty;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +17,38 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo) {
+		return args -> {
+			serverRepo.save(new Server(null,
+					"192.168.0.160",
+					"Ubuntu Linux",
+					"16 GB",
+					"Personal PC",
+					"http://localhost:8080/server/image/1.png",
+					Status.SERVER_UP));
+			serverRepo.save(new Server(null,
+					"192.168.0.58",
+					"Fedora Linux",
+					"16 GB",
+					"Dell Tower",
+					"http://localhost:8080/server/image/2.png",
+					Status.SERVER_DOWN));
+			serverRepo.save(new Server(null,
+					"192.168.1.21",
+					"MS 2008",
+					"32 GB",
+					"Web Server",
+					"http://localhost:8080/server/image/3.png",
+					Status.SERVER_UP));
+			serverRepo.save(new Server(null,
+					"192.168.0.160",
+					"Red Hat Enterprise",
+					"12 TB",
+					"Mail Server",
+					"http://localhost:8080/server/image/4.png",
+					Status.SERVER_DOWN));
+
+		};
+	}
 }
