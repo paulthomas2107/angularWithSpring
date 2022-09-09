3c96f6f137726cd3266d37103ef8988d37677446
Paul Thomas <paulthomas2107@gmail.com>
2022-09-09 13:49:30 +0100

    More.....

--- src/main/java/io/getarrays/server/model/Response.java
+++ src/main/java/io/getarrays/server/model/Response.java
@@ -1,10 +1,19 @@
 package io.getarrays.server.model;
 
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDate;
+import java.util.Map;
 
+@Data
+@SuperBuilder
+@JsonInclude(NON_NULL)
 public class Response {
 
     protected LocalDate timeStamp;
@@ -12,5 +21,7 @@ public class Response {
     protected HttpStatus status;
     protected String reason;
     protected String message;
+    protected String developerMessage;
+    protected Map<?, ?> data;
 
 }
---
+++ src/main/java/io/getarrays/server/resource/ServerResource.java
@@ -0,0 +1,77 @@
+package io.getarrays.server.resource;
+
+import static io.getarrays.server.enumerations.Status.*;
+import io.getarrays.server.model.Response;
+import io.getarrays.server.model.Server;
+import io.getarrays.server.service.implementation.ServerServiceImpl;
+import lombok.RequiredArgsConstructor;
+import static org.springframework.http.HttpStatus.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.IOException;
+
+import static java.time.LocalDate.*;
+import static java.util.Map.*;
+
+@RestController
+@RequestMapping("/server")
+@RequiredArgsConstructor
+public class ServerResource {
+
+    private final ServerServiceImpl serverService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Response> getServer() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("servers", serverService.list(30)))
+                        .message("Servers retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("server", server))
+                        .message(server.getStatus() == SERVER_UP ? "Ping OK" : "Ping failed !")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("server", serverService.create(server)))
+                        .message("Server created...")
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getServer(@PathVariable("id") Long id)  {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("server", serverService.get(id)))
+                        .message("Server retrieved...")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+}
